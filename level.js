510ecb857f2c7a64031657c3f3f8e77c2883a1a1
ksd42 <ksd42@njit.edu>
2024-02-22 14:12:31 -0500

    level.js

---
+++ level.js
@@ -0,0 +1,21 @@
+// level.js
+class Level {
+    constructor(scene) {
+        this.scene = scene;
+    }
+
+    buildLevel() {
+        this.createBackground();
+        this.createEntities();
+        this.buildFloor();
+    }
+
+    createBackground() {
+        this.scene.add.image(400, 300, 'lvl_default_bg');
+    }
+
+    createEntities() {
+        this.player = new Player(this.scene, 400, 400);
+        this.barrel = new Barrel(this.scene, 600, 300);
+    }
+}
