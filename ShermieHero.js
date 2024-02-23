fe6e727cbebefb50ca10d1e2f0d6e603b99e397d
Seniz Ozdemir <so9@njit.edu>
2024-02-22 20:51:19 -0500

    Note creation function

--- ShermieHero.js
+++ ShermieHero.js
@@ -7,6 +7,7 @@ class ShermieHero extends Level{
         this.createBackground();
         this.createEntities();
         this.createKeys();
+        this.create();
     }
 
     createBackground() {
@@ -33,6 +34,55 @@ class ShermieHero extends Level{
         this.down = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
         this.left = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         this.right = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    create(){
+        this.string1NoteTimings = [];
+        this.string2NoteTimings = [];
+        
+
+        this.noteTime = 10000; //ms
+        this.notes = [];
+        this.createNote(1);
+        this.createNote(2);
+        this.createNote(3);
+        this.createNote(4);
+
+    }
+
+    createNotes(){
+        
+    }
+
+    createNote(stringNum){
+        let note;
+        switch(stringNum){
+            case 1:
+                note = this.scene.add.circle(245, 0, 20, 0x03fc03);
+                this.notes.push(note);
+                this.scene.physics.add.existing(note);
+                this.scene.physics.moveTo(note, 245, 768, null, this.noteTime);
+                break;
+            case 2:
+                note = this.scene.add.circle(305, 0, 20, 0x03fc03);
+                this.notes.push(note);
+                this.scene.physics.add.existing(note);
+                this.scene.physics.moveTo(note, 305, 768, null, this.noteTime);
+                break;
+            case 3:
+                note = this.scene.add.circle(365, 0, 20, 0x03fc03);
+                this.notes.push(note);
+                this.scene.physics.add.existing(note);
+                this.scene.physics.moveTo(note, 365, 768, null, this.noteTime);
+                break;
+            case 4:
+                note = this.scene.add.circle(425, 0, 20, 0x03fc03);
+                this.notes.push(note);
+                this.scene.physics.add.existing(note);
+                this.scene.physics.moveTo(note, 425, 768, null, this.noteTime);
+                break;
+        }
+        
     }
 
     update(){
