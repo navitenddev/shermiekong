cd14699f9334c6fef6d9dc92fceb5dddf9702e5b
Seniz Ozdemir <so9@njit.edu>
2024-03-02 18:45:26 -0500

    Refactor: level now extends phaser scene, player extends phaser sprite

--- player.js
+++ player.js
@@ -1,12 +1,12 @@
 // player.js
-class Player extends Entity {
+class Player extends Phaser.Physics.Arcade.Sprite {
+
     constructor(scene, x, y) {
-        // Call the parent constructor (aka inherit from Entity)
         super(scene, x, y, 'player');
 
         // Set up any additional configurations (specific to the Player class)
-        this.sprite.setScale(0.25);
-        this.sprite.setCollideWorldBounds(true);
+        this.setScale(0.25);
+        this.setCollideWorldBounds(true);
 
         this.scene.physics.world.enable(this.sprite);
         this.sprite.body.setGravityY(300);
@@ -16,16 +16,16 @@ class Player extends Entity {
         this.isClimbing = false;
     }
 
-    update() {
-        super.update();
+    // update() {
+    //     super.update();
 
-        if (this.isClimbing) {
-            this.handleClimbing();
-        } 
-        else {
-            this.handlePlayerMovement();
-        }
-    }
+    //     if (this.isClimbing) {
+    //         this.handleClimbing();
+    //     } 
+    //     else {
+    //         this.handlePlayerMovement();
+    //     }
+    // }
 
     handlePlayerMovement() {
         if (this.cursors.left.isDown) {
@@ -40,6 +40,13 @@ class Player extends Entity {
 
         if (this.cursors.up.isDown && this.sprite.body.blocked.down) {
             this.sprite.setVelocityY(-350);
+        }
+        
+        if (this.isClimbing) {
+            this.handleClimbing();
+        } 
+        else {
+            this.handlePlayerMovement();
         }
     }
 
