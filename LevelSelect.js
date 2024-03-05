5b742285c279ca5796c4e9541b386f057cc8cfe1
Seniz Ozdemir <so9@njit.edu>
2024-03-05 17:45:33 -0500

    Implemented basic menu screen with level selector

---
+++ LevelSelect.js
@@ -0,0 +1,13 @@
+class LevelSelect extends Phaser.Scene{
+    constructor(){
+        super("LevelSelect");
+    }
+
+    create(){
+        this.add.text(20, 50, "Level 1").setInteractive()
+        .on('pointerdown', () => {this.scene.start("level1")});
+
+        this.add.text(20, 80, "Shermie Hero").setInteractive()
+        .on('pointerdown', () => {this.scene.start("ShermieHero")});
+    }
+}
