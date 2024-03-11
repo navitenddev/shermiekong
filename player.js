a7534b7dc3155297381b5aa6245abdbc04e4656a
Kareem Hegazy <kthegazy2002@gmail.com>
2024-03-10 20:39:45 -0400

    Jettpack Powerup

--- player.js
+++ player.js
@@ -12,6 +12,9 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         // Setup input handling for player movement
         this.cursors = scene.input.keyboard.createCursorKeys();
         this.isClimbing = false;
+        this.hasJettpack = false;
+        this.VelocityX = 200;
+        this.VelocityY = 350;
     }
 
     playerClimbing() {
@@ -24,18 +27,28 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     }
 
     handlePlayerMovement() {
-        if (this.cursors.left.isDown) {
-            this.setVelocityX(-200);
-        }
-        else if (this.cursors.right.isDown) {
-            this.setVelocityX(200);
+        if(this.hasJettpack) {
+            this.VelocityX = 400;
+            this.VelocityY = 400;
         }
         else {
-            this.setVelocityX(0);
+            this.VelocityX = 200;
+            this.VelocityY = 350;
+        }
+
+        if (this.cursors.left.isDown) {
+            this.setVelocityX(this.VelocityX * -1);
+        }
+        else if (this.cursors.right.isDown) {
+            this.setVelocityX(this.VelocityX);
+        }
+        else {
+            this.VelocityX = 0;
+            this.setVelocityX(this.VelocityX);
         }
 
         if (this.cursors.up.isDown && this.body.blocked.down) {
-            this.setVelocityY(-350);
+            this.setVelocityY(this.VelocityY * -1);
         }
     }
 
