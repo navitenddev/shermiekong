1eea5703b1ed9144af9d77b74e0e9757cad6d65f
Julian-Hame <jhamehs@gmail.com>
2024-03-11 00:24:29 -0400

    Restored level 1 to proper condition

--- level1.js
+++ level1.js
@@ -3,12 +3,10 @@ class Level1 extends Phaser.Scene {
     constructor() {
         super("level1");
     }
-
     preload() {
         // Level Backgrounds
         this.load.image('lvl_default_bg', 'assets/lvl-default-bg.png');
         this.load.image('lvl_1_bg', 'assets/lvl-1-bg.png');
-
         // Level Entities
         this.load.image('wolf', 'assets/wolf.png');
         this.load.image('platform', 'assets/platform.png');
@@ -16,32 +14,26 @@ class Level1 extends Phaser.Scene {
         this.load.image('girder', 'assets/girder.png');
         this.load.image('ladder', 'assets/ladder.png');
     }
-
     create() {
         this.createBackground();
         this.createEntities();
         // Set up collision between player and the barrel
         //this.physics.add.collider(currentLevel.player.sprite, currentLevel.barrel.sprite, this.handleCollision, null, this);
     }
-
     update() {
         this.player.handlePlayerMovement();
-        // this.barrel.update();
+        this.barrel.update();
     }
-
     buildLevel() {
         this.createBackground();
         this.createEntities();
     }
-
     createBackground() {
         this.add.image(400, 300, 'lvl_default_bg');
     }
-
     createEntities() {
         this.player = new Player(this, 100, 700);
-        //this.barrel = new Barrel(this, 600, 200);
-
+        this.barrel = new Barrel(this, 600, 200);
         var floor = this.physics.add.staticGroup();
         // 1st floor
         var x = 24;
@@ -104,23 +96,20 @@ class Level1 extends Phaser.Scene {
             x = x - 48;
         }
 
-        this.physics.add.collider(this.player.sprite, floor);
-        //this.physics.add.collider(this.barrel.sprite, floor);
-
+        this.physics.add.collider(this.player, floor);
+        this.physics.add.collider(this.barrel.sprite, floor);
         var ladders = this.physics.add.staticGroup();
         ladders.create(425, 700, 'ladder');
         ladders.create(225, 500, 'ladder');
-
         this.physics.add.collider(ladders, floor);
 
         // Add an overlap event to detect when the player is on the ladder
-        this.physics.add.overlap(this.player.sprite, ladders, this.handlePlayerClimbing, null, this);
+        this.physics.add.overlap(this.player, ladders, this.handlePlayerClimbing, null, this);
     }
 
     handlePlayerClimbing() {
-        //this.player.isClimbing = true;
+        this.player.isClimbing = true;
     }
-
     
     handleCollision(player, barrel) {
         // Perform specific actions when the player collides with a barrel
