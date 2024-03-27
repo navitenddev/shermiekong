4187b3b51a420dea6be7629c21271daa5724ecc4
Seniz Ozdemir <so9@njit.edu>
2024-03-26 21:57:04 -0400

    Returned player speed to previous values

--- player.js
+++ player.js
@@ -96,8 +96,8 @@ class Player extends Phaser.Physics.Arcade.Sprite {
             this.VelocityY = 400;
         }
         else {
-            this.VelocityX = 300; //120
-            this.VelocityY = 400; //230
+            this.VelocityX = 120; //120
+            this.VelocityY = 230; //230
         }
 
         if (this.cursors.left.isDown) {
