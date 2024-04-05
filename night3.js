8ad342bf95dcb3577763cdb13a52fcc431addf75
Seniz Ozdemir <so9@njit.edu>
2024-04-05 15:36:35 -0400

    Added necessary files

---
+++ night3.js
@@ -0,0 +1,67 @@
+class Night3 extends Phaser.Scene{
+    constructor(){
+        super("night3");
+    }
+    preload(){
+        this.load.image('dialogue', 'assets/dialogue_box.png');
+    }
+
+    create(){
+        const { previousHearts } = this.scene.settings.data;
+        console.log("interlude prev: " + previousHearts);
+        this.hearts = previousHearts;
+
+        this.add.image(336, 384, 'night');
+
+        this.song = this.sound.add("song2");
+        this.song.volume = 1;
+        this.song.loop = true;
+        this.song.play();
+
+        // dialogue should pop up after a moment
+        this.timedEvent = this.time.addEvent({
+            delay: 1000,
+            callback: this.dialogueInit,
+            callbackScope: this,
+            loop: false
+        });
+
+        //pause button
+        this.add.image(625, 40, 'pause_button')
+        .setScale(0.5)
+        .setInteractive()
+        .on('pointerdown', () => {this.pause()});
+    }
+
+    dialogueInit(){
+        this.lineNum = 0;
+        this.line = this.add.text(100, 620, "Time for my favorite part of the day...");
+        this.line.setDepth(3);
+        this.box = this.add.image(336, 600, "dialogue")
+        .setInteractive()
+        .on('pointerdown', () => {this.nextLine()});
+    }
+
+    nextLine(){
+        this.line.destroy();
+        this.lineNum += 1;
+        switch(this.lineNum){
+            case 1:
+                this.line = this.add.text(100, 620, "Maybe I should start keeping a dream journal.");
+                break;
+            case 2:
+                this.line = this.add.text(100, 620, "I've started looking forward to dreaming at night.");
+                break;
+            case 3:
+                this.song.stop();
+                this.scene.start('level4', { previousHearts: this.hearts });
+                break;
+        }
+    }
+
+    pause(){
+        this.scene.launch('pause');
+        this.scene.pause();
+    }
+
+}
